c9256fc7a352b883de37fc0609ff376b783043d3
lizgzil <lizgzil@hotmail.com>
2023-10-03 17:42:23 +0100

    Add files for prodigy annotation

---
+++ asf_floorplan_interpreter/pipeline/annotation/README.md
@@ -0,0 +1,74 @@
+# Using Prodigy to tag entities
+
+Activate the environment, if you need to run the Prodigy commands you will also need to do:
+
+```
+pip install prodigy -f https://[YOUR_LICENSE_KEY]@download.prodi.gy
+```
+
+Download the models to `outputs/models/rooms-model/best.pt` and `outputs/models/windows-doors-model/best.pt`.
+
+## Data
+
+Download and format the floorplan data into the jsonl file format needed for Prodigy annotation:
+
+```
+python asf_floorplan_interpreter/pipeline/annotation/create_images_data.py
+
+```
+
+This will create the file `asf_floorplan_interpreter/pipeline/annotation/floorplans.jsonl`.
+
+## Rooms:
+
+Annotate bounding boxes for each:
+
+```
+prodigy image.manual room_type_dataset asf_floorplan_interpreter/pipeline/annotation/floorplans.jsonl --loader jsonl --label BEDROOM,KITCHEN,BATHROOM,LIVING,STAIRCASE,OTHER --no-fetch
+
+```
+
+```
+prodigy db-out room_type_dataset > asf_floorplan_interpreter/pipeline/annotation/prodigy_labelled/room_type.jsonl
+
+```
+
+Use this to train a model.
+
+## Windows and doors:
+
+Since we have a model for this and its a long annotation task to tag every one, we can use the model to help us label.
+
+Manually correct Adeola's original model for doors and windows?
+
+```
+prodigy classify-images window_doors_dataset asf_floorplan_interpreter/pipeline/annotation/floorplans.jsonl WINDOW,DOOR -F asf_floorplan_interpreter/pipeline/annotation/floorplan_recipe.py
+
+```
+
+```
+prodigy db-out window_doors_dataset > asf_floorplan_interpreter/pipeline/annotation/prodigy_labelled/window_door.jsonl
+```
+
+## Everything model:
+
+This will use the pretrained models to help label ROOM, WINDOW, DOOR.
+
+```
+prodigy classify-everything-images room_window_door_dataset asf_floorplan_interpreter/pipeline/annotation/floorplans.jsonl WINDOW,DOOR,ROOM -F asf_floorplan_interpreter/pipeline/annotation/floorplan_recipe.py
+
+```
+
+```
+prodigy db-out room_window_door_dataset > asf_floorplan_interpreter/pipeline/annotation/prodigy_labelled/room_window_door.jsonl
+```
+
+## Clean up
+
+YOLO reads and saves each floorplan jpg, so you will end up with the directory being full of these. To clean them up run:
+
+```
+rm *.jpg
+```
+
+careful to be in the asf_floorplan_interpreter parent directory.
---
+++ asf_floorplan_interpreter/pipeline/annotation/floorplan_recipe.py
@@ -0,0 +1,81 @@
+import prodigy
+from prodigy.components.loaders import JSONL
+from ultralytics import YOLO
+
+
+def load_model(directory):
+    """Function to load YOLO model trained on floor plans annotated with polygons identifying rooms"""
+    return YOLO(directory)
+
+
+def yolo_2_segments(results):
+    segments = []
+    for (x, y, w, h), label in zip(results[0].boxes.xywh, results[0].boxes.cls):
+        x_min = x.item() - (w.item() / 2)
+        y_min = y.item() - (h.item() / 2)
+        x_max = x.item() + (w.item() / 2)
+        y_max = y.item() + (h.item() / 2)
+        segment = [[x_min, y_min], [x_max, y_min], [x_max, y_max], [x_min, y_max]]
+        segments.append(
+            {
+                "label": results[0].names[label.item()],
+                "points": segment,
+                "type": "polygon",
+            }
+        )
+    return segments
+
+
+@prodigy.recipe("classify-images")
+def classify_images(dataset, source, label):
+    model = load_model("outputs/models/windows-doors-model/best.pt")
+
+    def predict(stream, model=model):
+        for eg in stream:
+            results = model(eg["image"])
+            span_predictions = yolo_2_segments(results)
+            eg["spans"] = span_predictions
+            yield eg
+
+    stream = JSONL(source)
+
+    stream = predict(stream, model=model)
+
+    return {
+        "dataset": dataset,
+        "stream": stream,
+        "view_id": "image_manual",
+        "config": {
+            "labels": label.split(","),
+        },
+    }
+
+
+@prodigy.recipe("classify-everything-images")
+def classify_images(dataset, source, label):
+    window_door_model = load_model("outputs/models/windows-doors-model/best.pt")
+    room_model = load_model("outputs/models/rooms-model/best.pt")
+
+    def predict(stream, window_door_model=window_door_model, room_model=room_model):
+        for eg in stream:
+            window_door_results = window_door_model(eg["image"])
+            span_predictions = yolo_2_segments(window_door_results)
+
+            room_results = room_model(eg["image"])
+            span_predictions += yolo_2_segments(room_results)
+
+            eg["spans"] = span_predictions
+            yield eg
+
+    stream = JSONL(source)
+
+    stream = predict(stream, window_door_model=window_door_model, room_model=room_model)
+
+    return {
+        "dataset": dataset,
+        "stream": stream,
+        "view_id": "image_manual",
+        "config": {
+            "labels": label.split(","),
+        },
+    }
