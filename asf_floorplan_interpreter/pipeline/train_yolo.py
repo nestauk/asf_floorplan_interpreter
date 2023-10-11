cd22fd4b2310313774c6a1115e9644adeaa1ab8a
lizgzil <lizgzil@hotmail.com>
2023-10-11 17:32:09 +0100

    clean up flow

--- asf_floorplan_interpreter/pipeline/train_yolo.py
+++ asf_floorplan_interpreter/pipeline/train_yolo.py
@@ -1,8 +1,3 @@
-#### ONLY TO BE TRAINED ON GPU
-### USE IF A RERUN OF TRAINING - CURRENT MODEL WEIGHTS IN S3 BUCKET
-## ADD FUNCTION TO SAVE WEIGHTS ON S3
-
-# code to install requirements on batch machine
 import os
 
 try:
@@ -14,57 +9,6 @@ except:
     pass
 
 from metaflow import FlowSpec, project, step, Parameter, batch, conda
-
-# def train_yolo_model(epoch_number):
-#     """Train YOLO model locally"""
-
-#     model.train(
-#         data=(PROJECT_DIR / "asf_floorplan_interpreter/config/config.yaml"), epochs=epoch_number, imgsz=640
-#     )
-#     # HOW TO SAVE TRAINED MODEL?? IS THERE AN ARG TO DIRECT OUTPUTS? MAYBE PUT OUTPUS IN AN S3 OUTPUT FOLDER
-#     # AND DIRECT THE WEIGHTS FOR MODELS TO ANOTHER
-
-
-# def mini_model(config_file, epochs):
-#     model = YOLO("yolov8n-seg.pt")
-#     results = model.train(
-#             batch=8,
-#             device="cpu",
-#             data=config_file,
-#             epochs=epochs,
-#             imgsz=120,
-#         )
-#     return results
-
-
-# if __name__ == '__main__':
-#     model = YOLO("yolov8n-seg.pt")  # load a pretrained model (recommended for training)
-
-#     epochs = 2
-
-#     results = model.train(
-#         data=(PROJECT_DIR / "asf_floorplan_interpreter/config/config.yaml"), epochs=epochs, imgsz=640, batch=16, device="cpu",
-#         patience=3,
-#         save_dir="liztest/here/",
-#         project="lizproject"
-#     )
-
-# def pip(libraries):
-#     # From https://github.com/Netflix/metaflow/issues/24
-#     def decorator(function):
-#         @functools.wraps(function)
-#         def wrapper(*args, **kwargs):
-#             import subprocess
-#             import sys
-
-#             for library, version in libraries.items():
-#                 print('Pip Install:', library, version)
-#                 subprocess.run([sys.executable, '-m', 'pip', 'install', '--quiet', library + '==' + version])
-#             return function(*args, **kwargs)
-
-#         return wrapper
-
-#     return decorator
 
 
 class FloorPlanYolo(FlowSpec):
